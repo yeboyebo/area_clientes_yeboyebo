83bd4792adf5c346f8d119a8df0430303fbb2962
xavikortes <javiercortesveliz@gmail.com>
2019-07-25 12:03:59 +0200

    Cambios util.translate

--- model_flcliarea__models.py
+++ model_flcliarea__models.py
@@ -13,13 +13,13 @@ models.Field._miextend = _miextend
 
 
 class mtd_ac_usuarios(models.Model, BaseModel):
-    idusuario = models.AutoField(db_column="idusuario", verbose_name=FLUtil.translate(u"Identificador", u"MetaData"), primary_key=True)._miextend(visiblegrid=False, OLDTIPO="SERIAL")
+    idusuario = models.AutoField(db_column="idusuario", verbose_name=FLUtil.translate("MetaData", "Identificador"), primary_key=True)._miextend(visiblegrid=False, OLDTIPO="SERIAL")
     nombre = models.CharField(blank=False, null=True, max_length=100)._miextend(OLDTIPO="STRING")
     password = models.TextField(blank=False, null=True)._miextend(OLDTIPO="STRING")
-    esadmin = models.NullBooleanField(db_column="esadmin", verbose_name=FLUtil.translate(u"Admin", u"MetaData"), default=False, null=True)._miextend(OLDTIPO="BOOL")
+    esadmin = models.NullBooleanField(db_column="esadmin", verbose_name=FLUtil.translate("MetaData", "Admin"), default=False, null=True)._miextend(OLDTIPO="BOOL")
     codfuncional = models.CharField(blank=True, null=True, max_length=15)._miextend(OLDTIPO="STRING")
 
     class Meta:
         managed = True
-        verbose_name = FLUtil.translate(u"Usuarios area clientes", u"MetaData")
+        verbose_name = FLUtil.translate("MetaData", "Usuarios area clientes")
         db_table = 'ac_usuarios'
